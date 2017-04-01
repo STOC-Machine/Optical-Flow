418d5a42769627b352d1711293e9b8588e15580f
Dancinggremlin <malcolmcgrossman@gmail.com>
2017-04-01 10:24:13 -0500

    fixing color detection

--- color_detection.py
+++ color_detection.py
@@ -1,14 +1,21 @@
+# Author: Malcolm Grossman
+# This code looks for an object of certain color(s) on the screen and tracks it
+# The 
 import cv2
 import numpy as np
-
+from collections import deque
+import argparse
+# constructs and parses pare arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
+args = vars(ap.parse_args())
 # lower and upper limits for black (the color of the rumba)
-black_lower = (0,0,0)
-black_upper = (40, 40, 40)
+color_lower = (29,86,6)
+color_upper = (64, 255, 255)
 #initialize tracker points and coordinate deltas
 pts = deque(maxlen=args["buffer"])
-counter = 0
-(dX, dY) = (0,0)
-direction = ""
+
 
 # Startup of camera, has failsafe in case nothing is returned from cv2.VideoCatpture
 cap = cv2.VideoCapture(0)
@@ -24,7 +31,7 @@ while(True):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # constructs a mask for the color black and then removes any blobs
-    mask = cv2.inRange(hsv, black_lower, black_upper)
+    mask = cv2.inRange(hsv, color_lower, color_upper)
     mask = cv2.erode(mask, None, iterations = 2)
     mask = cv2.dilate(mask, None, iterations = 2)
 
@@ -43,40 +50,20 @@ while(True):
         if radius > 10:
             cv2.circle(frame, (int(x), int(y)), int(radius), (0,255,255),2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
-            pts.appendleft(center)
-        # loop over tracked points
-        for i in np.arange(1, len(pts)):
-            # if tracked points are None ignore them
-            if pts[i-1] is None or pts[i] is None:
-                continue
-            #check if points accumulated in buffer
-            if counter >= 10 and i == 1 and pts[-10] is not None:
-                # get difference between x and y and re-initialize the
-                # direction text var
-                dX = pts[-10][0] - pts[i][0]
-                dY = pts[-10][1] - pts[i][1]
-                (dirX, dirY) = ("", "")
-                #make sure there's actually movement in x and y direcitons
-                if np.abs(dX) > 20:
-                    dirX = "East" if np.sign(dY) == 1 else "West"
-                if np.abs(dY) > 20:
-                    dirY = "North" if np.sign(dY) == 1 else "South"
-
-                if dirX != "" and dirY != "":
-                    direction = "{}-{}".format(dirY, dirX)
-
-                else:
-                    direction = dirX if dirX != "" else dirY
-
-                thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-                cv2.line(frame, pts[i-1], pts[i], (0,0,255), thickness)
+    
+    pts.appendleft(center)
+    # loop over tracked points
+    for i in np.arange(1, len(pts)):
+        # if tracked points are None ignore them
+        if pts[i-1] is None or pts[i] is None:
+            continue
+        #draw the lines
+        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i-1], pts[i], (0,0,255), thickness)
                 
-            cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0,0,255),3)
-            cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY), (10, frame.shape[0]-10), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.35, (0,0,255), 1)
        
     cv2.imshow("Color Detector", frame)
-    counter += 1
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
