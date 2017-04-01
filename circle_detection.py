f1e870c1c4ad53f8770b2b0e7d19e25f1747d199
Dancinggremlin <malcolmcgrossman@gmail.com>
2017-04-01 11:41:17 -0500

    fine tuned circle detection algorithm

--- circle_detection.py
+++ circle_detection.py
@@ -9,22 +9,25 @@ else:
     ret = False
 while(True):
     ret, frame = cam.read()
-    img = cv2.medianBlur(frame,15)
-    imgg = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)        
-    circles = cv2.HoughCircles(imgg,cv2.HOUGH_GRADIENT,1,20,
-                               param1=50,param2=30,minRadius=5,maxRadius=20)
-    if circles is None:
-       cv2.imshow("Circle Detection", frame)
-       continue
-    
-    circles = np.uint16(np.around(circles))
-    for i in circles[0,:]:
-        # draw the outer circle
-        cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
-        # draw the center of the circle
-        cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
+    img = cv2.medianBlur(frame,5)
+    imgg = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
+    circles = cv2.HoughCircles(imgg,cv2.HOUGH_GRADIENT,1,75,
+                               param1=15,param2=75, minRadius=1,maxRadius=100)
+    print(circles)
+
+    
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            # draw the outer circle
+            cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
+            # draw the center of the circle
+            cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
+
+    
     cv2.imshow("Circle Detection",frame)
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cam.release()
