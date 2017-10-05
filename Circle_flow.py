9ca6da70d90b24e80be127141531050933f4da18
Dancinggremlin <malcolmcgrossman@gmail.com>
2017-10-05 13:38:36 -0500

    Grammar & Time
    Fixed some placement/grammar issues to make it loom nicer. Also fixed
    time so it gets more acurate intervals between circle detection.

--- Circle_flow.py
+++ Circle_flow.py
@@ -1,15 +1,18 @@
-#Logitech 720P HD camera has resolution = (640 x 480)
+# Logitech 720P HD camera has resolution = (640 x 480)
+import math
+import time
+from operator import attrgetter
+
 import cv2
 import numpy as np
-import time
+
 import GridSquares
-import math
-from operator import attrgetter
 
 lk_params = {'winSize': (15, 15), 'maxLevel': 2,
              'criteria': (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)}
 
 feature_params = {'maxCorners': 500, 'qualityLevel': 0.3, 'minDistance': 7, 'blockSize': 7}
+
 
 class App:
     def __init__(self):
@@ -18,7 +21,7 @@ class App:
         self.tracks = []
         self.velocitor = [] # speed in pixels/second
         self.velocity = []  # speed in centimeters/second
-        self.cam = cv2.VideoCapture(1)
+        self.cam = cv2.VideoCapture(0)
         # camera is listed as having an FOV of 60 degrees
         # DFOV = Diagonal Field of View
         # Horizontal FOV = 2 * atan(tan(DFOV/2)*cos(atan(9/16)))
@@ -46,12 +49,12 @@ class App:
             frame_copy = np.copy(frame)
             squares = GridSquares.computeFrameSquares(frame_copy)
             if self.calibration_values is None:
-                self.calibration_values = cv2.calibrationMatrixValues(np.array([[811.75165344, 0., 317.03949866],[0., 811.51686214, 247.65442989],[0., 0., 1.]]), (640, 480), 3,3)
+                self.calibration_values = cv2.calibrationMatrixValues(
+                    np.array([[811.75165344, 0., 317.03949866], [0., 811.51686214, 247.65442989], [0., 0., 1.]]),
+                    (640, 480), 3, 3)
                 self.HFOV = self.calibration_values[0]*(math.pi/180)
                 self.VFOV = self.calibration_values[1]*(math.pi/180)
             img = cv2.medianBlur(frame, 5)
-            t = time.clock()
-            self.times.append(t)
             frame_gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
             vis = frame.copy()
             circles = cv2.HoughCircles(frame_gray, cv2.HOUGH_GRADIENT, 1, 75,
@@ -65,6 +68,7 @@ class App:
                     p1, st, err = cv2.calcOpticalFlowPyrLK(img0, img1, p0, None, **lk_params)
                     p0r, st, err = cv2.calcOpticalFlowPyrLK(img1, img0, p1, None, **lk_params)
                     t2 = time.clock()
+                    self.times.append((t1,t2))
                     d = abs(p0-p0r).reshape(-1,2).max(-1)
                     good = d < 1
                     new_tracks = []
@@ -81,12 +85,13 @@ class App:
                     self.tracks = new_tracks
                     cv2.polylines(vis, [np.int32(tr) for tr in self.tracks], False, (0, 255, 0))
                     for i in range(len(self.tracks)-1):
-                        tracklen = len(self.tracks[i])-1
-                        x1=self.tracks[i][tracklen][0]-self.tracks[i][0][0]
-                        y1=self.tracks[i][tracklen][1]-self.tracks[i][0][1]
-                        self.velocitor.append([x1/(self.times[self.frame_idx-1]-self.times[self.frame_idx]),y1/(self.times[self.frame_idx-1]-self.times[self.frame_idx])])
-                        cv2.line(vis, (self.tracks[i][0][0], self.tracks[i][0][1]), (self.tracks[i][1][0], self.tracks[i][1][1]), (0,255,0),1)
-                        #print(self.velocitor[i])
+                        trackleangth = len(self.tracks[i])-1
+                        x1=self.tracks[i][trackleangth][0]-self.tracks[i][0][0]
+                        y1=self.tracks[i][trackleangth][1]-self.tracks[i][0][1]
+                        self.velocitor.append([x1 / (self.times[self.frame_idx][1] - self.times[self.frame_idx][0]),
+                                               y1 / (self.times[self.frame_idx][1] - self.times[self.frame_idx][0])])
+                        cv2.line(vis, (self.tracks[i][0][0], self.tracks[i][0][1]),
+                                 (self.tracks[i][1][0], self.tracks[i][1][1]), (0, 255, 0), 1)
                     if squares != []:
                         for square in squares:
                             square.getPosStats()
@@ -95,7 +100,6 @@ class App:
                                 square1.compareSquareNormals(square2,frame_copy)
                         squares.sort(key=attrgetter("score"),reverse=True)
                         height = squares[0].getHeight()
-                        print(height)
 
                         # x is the distance from one side of the screen to the other in cm
                         # y is the distance from the top to the bottom of the screen in cm
@@ -108,9 +112,11 @@ class App:
                         for j in range(len(self.velocitor) - 1):
                             # velocity = (x(cm)/x(pxls))*velocity(pxls/s)
                             self.velocity.append([self.velocitor[j][0] * (x / 640), self.velocitor[j][1] * (y / 480)])
+                """
                 if self.frame_idx % self.detect_interval == 0:
                     for i in circles[0,:]:
                         self.tracks.append([(i[0],i[1])])
+                """
                 for i in circles[0, :]:
                     cv2.circle(vis, (i[0],i[1]),i[2], (0,255,0),2)
                     cv2.circle(vis, (i[0],i[1]),2 , (0,0,255),3)
@@ -126,8 +132,6 @@ def main():
     """
     the main function, runs all code listed above
     """
-    print(1)
-    import sys
     print(__doc__)
     App().run()
     cv2.destroyAllWindows()
--- cameraStats.py
+++
@@ -1,7 +0,0 @@
-import cv2
-cam = cv2.VideoCapture(0)
-ret, frame = cam.read()
-height, width, channels =  frame.shape
-print(width)
-print(height)
-print(channels)
